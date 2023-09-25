a7902c0a4cefe137d6248dd72e9f10b59687929c
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2023-09-24 19:44:12 -0700

    Create GLFW window

--- src/viewer/main.cpp
+++ src/viewer/main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
+#define GLFW_INCLUDE_NONE
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+constexpr int SCREEN_WIDTH = 1280;
+constexpr int SCREEN_HEIGHT = 720;
+
+void handle_glfw_error(int error, const char* description) {
+    std::cerr << "GLFW Error " << error << ": " << description << std::endl;
+}
+
 int main(void) {
 
+    // Initialization
     std::cout << "Hello viewer!" << std::endl;
+
+    if (!glfwInit()) {
+        std::cerr << "GLFW initialization failed!" << std::endl;
+        return 1;
+    }
+    glfwSetErrorCallback(handle_glfw_error);
+
+    auto window = glfwCreateWindow(1280, 720, "Viewer Demo", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create window!" << std::endl;
+        return 1;
+    }
+
+
+
+
+    // Cleanup
+
+    if (window) {
+        glfwDestroyWindow(window);
+        window = nullptr;
+    }
+
+    glfwTerminate();
 
     return 0;
 }
